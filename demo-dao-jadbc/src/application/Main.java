aaae5fc5e9f2eabca299e15f297f39c7ba7c1e31
SamuelMax10 <samuelmax544@gmail.com>
2024-04-22 19:03:01 -0300

    feat: Department entity class

---
+++ demo-dao-jadbc/src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"Tecnologia da informação");
+        System.out.println(obj);
+    }
+}
