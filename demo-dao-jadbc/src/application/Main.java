dca42a63935fb7f4403b9896dc8d830577b85de0
SamuelMax10 <samuelmax544@gmail.com>
2024-04-23 19:48:59 -0300

    feat: SellerDaoJDBC and DaoFactory

--- demo-dao-jadbc/src/application/Main.java
+++ demo-dao-jadbc/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,7 +10,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Tecnologia da informação");
-        Seller seller = new Seller(21,"Samuel","samuel@gmail.com",new Date(),3000.00,obj);
+        Seller seller = new Seller(21, "Samuel", "samuel@gmail.com", new Date(), 3000.00, obj);
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
         System.out.println(seller);
     }
 }
