df985d8f1545d73b75603f569e2676767e31fc7e
SamuelMax10 <samuelmax544@gmail.com>
2024-05-18 15:47:19 -0300

    feat: Service layer, component registration

---
+++ Projeto_Spring/Projeto_Spring/src/main/java/com/ProjetoWeb/Projeto_Spring/service/UserService.java
@@ -0,0 +1,25 @@
+package com.ProjetoWeb.Projeto_Spring.service;
+
+import com.ProjetoWeb.Projeto_Spring.entities.User;
+import com.ProjetoWeb.Projeto_Spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
