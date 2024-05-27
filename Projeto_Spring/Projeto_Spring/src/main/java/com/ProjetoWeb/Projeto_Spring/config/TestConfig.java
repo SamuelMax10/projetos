e748e7e74a3821a1ecbde20dea7d544dd2555f92
SamuelMax10 <samuelmax544@gmail.com>
2024-05-27 18:52:05 -0300

    feat: created Many-to-many association with JoinTable

--- Projeto_Spring/Projeto_Spring/src/main/java/com/ProjetoWeb/Projeto_Spring/config/TestConfig.java
+++ Projeto_Spring/Projeto_Spring/src/main/java/com/ProjetoWeb/Projeto_Spring/config/TestConfig.java
@@ -32,6 +32,29 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        var c1 = new Category(null, "Eletronics");
+        var c2 = new Category(null, "Books");
+        var c3 = new Category(null, "Computers");
+
+        var p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 100.0, "");
+        var p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 3000.0, "");
+        var p3 = new Product(null, "Monitor LG 24 polegadas", "Nam eleifend maximus tortor, at mollis.", 660.0, "");
+        var p4 = new Product(null, "Teclado mecanico Redragon", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        var p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 200.0, "");
+
+        categoryRepository.saveAll((Arrays.asList(c1, c2, c3)));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p2.getCategories().add(c3);
+        p3.getCategories().add(c3);
+        p4.getCategories().add(c3);
+        p5.getCategories().add(c2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         var u = new User(null, "Samuel Max", "samuel@gmail.com", "4002-8022", "12345");
         var u1 = new User(null, "Sabrina Izabela", "sabrina@gmail.com", "7070-7080", "54321");
 
@@ -39,19 +62,7 @@ public class TestConfig implements CommandLineRunner {
         var o1 = new Order(null, Instant.parse("2024-05-20T20:10:00Z"), OrderStatus.WAITING_PAYMENT, u1);
         var o3 = new Order(null, Instant.parse("2024-05-20T21:30:00Z"), OrderStatus.WAITING_PAYMENT, u);
 
-        var c = new Category(null,"Eletronics");
-        var c1 = new Category(null,"Books");
-        var c2 = new Category(null,"Computers");
-
-        var p = new Product(null,"The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur." ,100.0,"");
-        var p1 = new Product(null,"Smart TV", "Nulla eu imperdiet purus. Maecenas ante." ,3000.0,"");
-        var p2 = new Product(null,"Monitor LG 24 polegadas", "Nam eleifend maximus tortor, at mollis." ,660.0,"");
-        var p3 = new Product(null,"Teclado mecanico Redragon", "Donec aliquet odio ac rhoncus cursus." ,1200.0,"");
-        var p4 = new Product(null,"Rails for Dummies", "Cras fringilla convallis sem vel faucibus." ,200.0,"");
-
         userRepository.saveAll(Arrays.asList(u, u1));
         orderRepository.saveAll(Arrays.asList(o, o1, o3));
-        categoryRepository.saveAll((Arrays.asList(c,c1,c2)));
-        productRepository.saveAll(Arrays.asList(p,p1,p2,p3,p4));
     }
 }
