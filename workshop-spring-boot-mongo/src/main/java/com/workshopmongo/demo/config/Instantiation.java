1a59d889333e716531fe44b7555cabb961d1cafa
SamuelMax10 <samuelmax544@gmail.com>
2024-07-02 20:18:18 -0300

    Feat: Referencing the user's posts

--- workshop-spring-boot-mongo/src/main/java/com/workshopmongo/demo/config/Instantiation.java
+++ workshop-spring-boot-mongo/src/main/java/com/workshopmongo/demo/config/Instantiation.java
@@ -34,17 +34,18 @@ public class Instantiation implements CommandLineRunner {
         postRepository.deleteAll();
 
         User samuel = new User(null, "Samuel Max", "samuel@gmail.com");
-
         User sabrina = new User(null, "Sabrina Izabela", "sabrina@gmail.com");
-
         User leidynara = new User(null, "Leidynara Breder", "leidynara@gmail.com");
 
         userRepository.saveAll(Arrays.asList(samuel, sabrina, leidynara));
 
         var post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para o Cabo Frio!!!", new AuthorDto( samuel));
-
         var post2 = new Post(null, sdf.parse("25/03/2024"), "Bom dia", "Me arrumando para ir a praia :)", new AuthorDto(samuel));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
+
+        samuel.getPosts().addAll(Arrays.asList(post1,post2));
+
+        userRepository.save(samuel);
     }
 }
